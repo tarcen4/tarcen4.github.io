95fdfb24dc9b1217dc3df89b61f8e8745a68889d
tarcen4 <48488066+tarcen4@users.noreply.github.com>
2019-03-25 17:24:19 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,7 +1,7 @@
 let Louisiana = L.map('Louisiana').setView([31, -92], 3)
 L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}/{y}.png').addTo(Louisiana)
 let LSU = L.marker([30.4133, -91.1800]).addTo(Louisiana)
-let Baton_Rouge = L.polygon([
+let polygon = L.polygon([
   [30.511415, -91.174133],
   [30.473722, -91.057859],
   [30.351835, -91.048093],
