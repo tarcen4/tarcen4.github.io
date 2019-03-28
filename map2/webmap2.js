20e7c4f6aab2b9a82345dc7c4df0c734f49662da
Therese M Arceneaux <tarcen4@lsu.edu>
2019-03-28 11:03:55 -0500

    1103

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,11 +1,11 @@
 let usa = L.map('webmap2').setView([38, -100], 4.4);
 L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(usa);
-L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/guidance_model_coastalocean_estofs_time/MapServer/WMSServer', {
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
   transparent: true,
   attribution: 'NOAA, Iowa State University'
-}).addTo(usa);
+}).addTo(usa)
 L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/guidance_model_coastalocean_estofs_time/MapServer/WMSServer', {
   layers: '1',
   format: 'image/png',
