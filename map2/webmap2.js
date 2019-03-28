c4f7fe9703b9fc10e62e59cf3f210470dc45ad32
tarcen4 <48488066+tarcen4@users.noreply.github.com>
2019-03-28 11:28:25 -0500

    Create webmap2.js
    11:28am

---
+++ map2/webmap2.js
@@ -0,0 +1,17 @@
+let usa = L.map('webmap2').setView([38, -100], 4.4);
+L.tileLayer('https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png').addTo(usa);
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/mrms_nn.cgi?', {
+  layers: 'mrms_p72h',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA, Iowa State University'
+}).addTo(usa)
+L.tileLayer.wms('https://nowcoast.noaa.gov/arcgis/services/nowcoast/guidance_model_coastalocean_estofs_time/MapServer/WMSServer', {
+  layers: '1',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'NOAA'
+}).addTo(usa);
+L.esri.dynamicMapLayer({
+  url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer'
+}).addTo(usa)
