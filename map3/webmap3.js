f16bbe1313e31849409661c254cd8c2ef97c295b
Therese M Arceneaux <tarcen4@lsu.edu>
2019-04-04 11:47:57 -0500

    3

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,9 +1,26 @@
-let nola = L.map('webmap3').setView([30.02, -89.9], 11);
+let nola = L.map('webmap3').setView([30, -89.9], 12);
 let basemap = 'https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png';
 L.tileLayer(basemap).addTo(nola);
-let nolaDemographicsUrl = 'https://opendata.arcgis.com/datasets/4593a994e7644bcc91d9e1c096df1734_0.geojson';
+let nolaDemographicsUrl = 'https://opendata.arcgis.com/datasets/3273a5f8334d40838681ff0337eddb8c_0.geojson';
 jQuery.getJSON(nolaDemographicsUrl, function (data) {
-  let stateStyle = { color: 'purple' }
-  let nolaGeojsonOptions = { style: stateStyle }
-  L.geoJSON(data, nolaGeojsonOptions).addTo(nola)
+  let parkStyle = function (feature) {
+    let acres = feature.properties.ACRES
+    let parkColor = 'purple'
+    if ( acres > 10  ) { parkColor = 'blue' }
+    return {
+      color: parkColor,
+      weight: 1.5,
+      fillOpacity: 0.3
+    }
+  }
+    let onEachFeature = function (feature, layer) {
+      let name = feature.properties.STATE_NAME
+      let age = feature.properties.MED_AGE
+      layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+  }
+  let parksGeojsonOptions = {
+    style: parkStyle,
+    onEachFeature: onEachFeature
+  }
+  L.geoJSON(data, parksGeojsonOptions).addTo(nola)
 })
