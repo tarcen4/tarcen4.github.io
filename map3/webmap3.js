fd45de77c308a0a6e288d3a5213200551b54b129
Therese M Arceneaux <tarcen4@lsu.edu>
2019-04-04 11:18:31 -0500

    3

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,8 +1,9 @@
-let usa = L.map('webmap3').setView([32.18, -99.14], 4);
+let nola = L.map('webmap3').setView([30.02, -89.9], 11);
 let basemap = 'https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png';
-L.tileLayer(basemap).addTo(usa);
-let stateDemographicsUrl = 'https://opendata.arcgis.com/datasets/931492aa51834f639a00a3bd32534113_0.geojson';
-jQuery.getJSON(stateDemographicsUrl, function (data) {
-  L.geoJSON(data).addTo(usa)
+L.tileLayer(basemap).addTo(nola);
+let nolaDemographicsUrl = 'https://opendata.arcgis.com/datasets/4593a994e7644bcc91d9e1c096df1734_0.geojson';
+jQuery.getJSON(nolaDemographicsUrl, function (data) {
+  let stateStyle = { color: 'purple' }
+  let nolaGeojsonOptions = { style: stateStyle }
+  L.geoJSON(data, nolaGeojsonOptions).addTo(nola)
 })
-L.geoJSON(data, { style: { color: 'green' } }).addTo(usa);
