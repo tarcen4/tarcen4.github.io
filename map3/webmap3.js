de8495b61a112722099a3231749f6aca87146ede
Therese M Arceneaux <tarcen4@lsu.edu>
2019-04-04 10:55:13 -0500

    3

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,4 +1,8 @@
 let usa = L.map('webmap3').setView([32.18, -99.14], 4);
-let basemap = 'https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png'
+let basemap = 'https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png';
 L.tileLayer(basemap).addTo(usa);
-let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
+let stateDemographicsUrl = 'https://opendata.arcgis.com/datasets/931492aa51834f639a00a3bd32534113_0.geojson';
+jQuery.getJSON(stateDemographicsUrl, function (data) {
+  L.geoJSON(data).addTo(usa)
+})
+L.geoJSON(data, { style: { color: 'green' } }).addTo(usa);
