f8cc0a10e8fe8d713e14f42149865e16d17bf209
Therese M Arceneaux <tarcen4@lsu.edu>
2019-04-09 11:36:51 -0500

    3

--- Final Project/webmap3.js
+++ Final Project/webmap3.js
@@ -1,12 +1,12 @@
-let nola = L.map('webmap3').setView([30, -89.9], 12);
+let nola = L.map('webmap3').setView([29.989260, -90.055408], 12);
 let basemap = 'https://maps.heigit.org/openmapsurfer/tiles/roads/webmercator/{z}/{x}/{y}.png';
 L.tileLayer(basemap).addTo(nola);
 let nolaDemographicsUrl = 'https://opendata.arcgis.com/datasets/3273a5f8334d40838681ff0337eddb8c_0.geojson';
 jQuery.getJSON(nolaDemographicsUrl, function (data) {
   let parkStyle = function (feature) {
-    let acres = feature.properties.ACRES
+    let council = feature.properties.COUNCIL
     let parkColor = 'purple'
-    if ( acres > 10  ) { parkColor = 'blue' }
+    if ( council=='A'  ) { parkColor = 'teal' }
     return {
       color: parkColor,
       weight: 1.5,
@@ -15,8 +15,10 @@ jQuery.getJSON(nolaDemographicsUrl, function (data) {
   }
     let onEachFeature = function (feature, layer) {
       let name = feature.properties.NAME
-      let acres = feature.properties.ACRES
-      layer.bindPopup('Name: ' + name + '<br>Acres:' + acres + '<br>National average: 38')
+      let street = feature.properties.STREET
+      let neighborhood = feature.properties.NEGIHBORHOOD_NAME
+      let council = feature.properties.COUNCIL
+      layer.bindPopup('Name: ' + name + '<br>Street: ' + street + '<br>Neighborhood: ' + neighborhood + '<br>City Council District: ' + council)
   }
   let parksGeojsonOptions = {
     style: parkStyle,
